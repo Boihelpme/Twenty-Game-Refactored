305dd433242ed0235c8d56fcaddc51afc6eb8605
Scott <jayscott@FVFZMYCBJ1WK.tld>
2022-04-13 13:45:50 -0500

    idk2

---
+++ script.js
@@ -0,0 +1,11 @@
+function run() {
+
+    getCard()
+}
+
+function getCard() {
+    var r = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C", "D", "H", "S"]
+    var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+}
