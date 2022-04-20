65d96bede4380727a6d2f2ba3ec9a18f9a2c0b7a
Scott <jayscott@FVFZMYCBJ1WK.tld>
2022-04-20 13:47:25 -0500

    Cards

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
  function run() {
 
-    getCard()
+    user()
+
 }
+
+
 
 
 function getCard() {
-    var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+    var suit =suitArray[randomSuit]
+    return suit
 }
+
+score = 0
+compc = 0
+
+function genNum() {
+    var A = Math.floor(Math.random() * 10) + 1
+    return A
+}
+
+function user() {
+    var B = getCard()
+    var C = genNum()
+    score = score + C
+    document.getElementById("paragraph").innerHTML = "User score is " + score; 
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + C + B + ".jpg>")
+    return score
+
+}
+
+
+
 
  
